c3ba512bc241801a5225b8c0155abd48cd288c7d
Jessela26 <jelbaniqued26@gmail.com>
2023-05-03 20:31:13 -0400

    Initial Commit

---
+++ PlantyShop/PlantyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantyShopApp.swift
+//  PlantyShop
+//
+//  Created by Jessela Emile on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
